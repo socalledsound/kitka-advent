88eda9a9971876f89d01ef79848405f749600c12
Chris Kubick <socalledsound@gmail.com>
2020-11-28 17:03:51 -0800

    fixed hour

--- js/time.js
+++ js/time.js
@@ -1,11 +1,13 @@
 function getImage(currentHour, day){
     console.log(currentHour);
-    if(currentHour < 6 || currentHour > 17){
+    if(currentHour < 6 || currentHour > 16){
+        console.log(currentHour, 'night');
         if(day === 31){
             return nightGreenImage
         } 
         return nightWinterImage; 
     } else {
+        console.log(currentHour, 'day');
         if(day === 31){
             return dayGreenImage
         } 
